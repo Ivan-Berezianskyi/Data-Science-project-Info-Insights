edf0014b01908f174fe14bd35cda42403afca472
Oleh Pona <oleh.pona.shi.2025@lpnu.ua>
2025-10-12 17:47:57 +0300

    init

---
+++ web/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div class="w-full h-dvh flex items-center justify-center bg-black">
+      <img class="rounded-full" src="/phenuchnuy.jpg" alt="phenuchnuy" />
+    </div>
+    
+  </div>
+</template>
